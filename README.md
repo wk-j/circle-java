27ca808685618ba46b0aac0c5d6a9c93e121521f
wk-j <somnuk.wk@gmail.com>
2019-08-03 15:08:27 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Circle Test
+
+```bash
+mkdir -p src/{main,test}/java/wk
+mvn package
+```
---
+++ src/test/java/wk/ProgramTests.java
@@ -0,0 +1,17 @@
+package wk;
+
+import org.junit.Test;
+
+public class ProgramTests {
+    @Test
+    public void a() {
+    }
+
+    @Test
+    public void b() {
+    }
+
+    @Test
+    public void c() {
+    }
+}
